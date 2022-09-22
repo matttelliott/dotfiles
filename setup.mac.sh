5273aa656ec16cd6f05f455a792b82776d02a5dc
matt <git@matttelliott.com>
2022-09-21 23:01:44 -0500

    rm zprezto runcoms

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+# Because Git submodule commands cannot operate without a work tree, they must
+# be run from within $HOME (assuming this is the root of your dotfiles)
+cd "$HOME"
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until bootstrap has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
 # Install dependencies rosetta, homebrew, stow, and git
 sudo softwareupdate --install-rosetta --agree-to-license
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
--- zsh/setup.mac.sh
+++ zsh/setup.mac.sh
@@ -1,10 +1,5 @@
 #!/usr/bin/env bash
 echo "Setup Zsh"
-chsh -s /bin/zsh
 cd $HOME/dotfiles
 stow zsh
 cd $HOME
-for file in zlogin zlogout zpreztorc zprofile zshenv zshrc
-do
-  ln -s -f ".zprezto/runcoms/$file" $HOME/.$file;
-done
