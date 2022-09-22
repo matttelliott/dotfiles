82d69a880145ab05766b458d03251896bc031f4f
matt <git@matttelliott.com>
2022-09-21 22:35:15 -0500

    init

--- setup.mac.sh
+++ setup.mac.sh
@@ -1,4 +1,13 @@
+#!/usr/bin/env bash
+
+# Install rosetta, homebrew, and git
 sudo softwareupdate --install-rosetta
+/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew install git
+# clone main repo
+git clone https://gitlab.com/matttelliott/dotfiles-stow.git dotfiles
+cd dotfiles
+# run setup scripts
 bash homebrew/setup.mac.sh
 bash git/setup.mac.sh
 bash neovim/setup.mac.sh
