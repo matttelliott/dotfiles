ad5d6ec8c0ea4855338e8754cbc0e5e9869e6aca
matt <git@matttelliott.com>
2022-09-21 22:22:03 -0500

    init

--- zsh/.zprezto/modules/prompt/init.zsh
+++ zsh/.zprezto/modules/prompt/init.zsh
@@ -8,8 +8,6 @@
 # Load and execute the prompt theming system.
 autoload -Uz promptinit && promptinit
 
-echo 'prompt/init'
-echo $prompt_argv
 # Load the prompt theme.
 zstyle -a ':prezto:module:prompt' theme 'prompt_argv'
 if [[ "$TERM" == (dumb|linux|*bsd*) ]] || (( $#prompt_argv < 1 )); then
@@ -18,5 +16,3 @@ else
   prompt "$prompt_argv[@]"
 fi
 unset prompt_argv
-
-# source $HOME/.promptline.sh
