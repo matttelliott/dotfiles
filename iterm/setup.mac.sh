bbf8a0759fd49f50668660824c025522954bdd9f
matt <git@matttelliott.com>
2022-09-24 06:49:38 -0500

    iterm

--- iterm/setup.mac.sh
+++ iterm/setup.mac.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 echo "********************Setup Iterm********************"
 cd "$HOME/dotfiles"
-stow iterm
 brew install iterm2
+rm -rf ~/.iterm2
+rm -rf ~/.config/iterm2
+stow iterm
