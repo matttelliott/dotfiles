efbd025051ed3a07266c5ad87e3d9b36ad6341df
matt <git@matttelliott.com>
2022-10-13 00:21:51 -0500

    fix neovim install?

--- neovim/setup.mac.sh
+++ neovim/setup.mac.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles"
-rm -rf $HOME/.local/share/nvim
 bat neovim/README.md
+rm -rf $HOME/.local/share/nvim
 stow neovim
 brew install neovim
-nvim +PackerSync +qa!
+nvim +PackerSync
 nvim +PackerSync +qa!
 nvim +PackerSync +qa!
 
