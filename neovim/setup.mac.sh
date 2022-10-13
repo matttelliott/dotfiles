90bab601a41da7275b4d8f247f8cdf97d5992e53
matt <git@matttelliott.com>
2022-10-13 00:10:00 -0500

    auto - matt@Matts-Air.lan

--- neovim/setup.mac.sh
+++ neovim/setup.mac.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles"
 rm -rf $HOME/.local/share/nvim
+bat neovim/README.md
 stow neovim
 brew install neovim
-nvim +PackerSync
-nvim +PackerSync
+nvim +PackerSync +qa!
+nvim +PackerSync +qa!
+nvim +PackerSync +qa!
 
 
