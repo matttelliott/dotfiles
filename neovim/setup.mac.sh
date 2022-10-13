2cf7b3aca6a72c0a08925eaeea1f121006eaa959
matt <git@matttelliott.com>
2022-10-13 16:46:17 -0500

    fuck stow

--- neovim/setup.mac.sh
+++ neovim/setup.mac.sh
@@ -2,7 +2,10 @@
 cd "$HOME/dotfiles"
 bat neovim/README.md
 rm -rf $HOME/.local/share/nvim
+rm -rf $HOME/.local/state/nvim
 stow neovim
+rm -rf $HOME/.local/share/nvim
+rm -rf $HOME/.local/state/nvim
 brew install neovim
 nvim +PackerSync
 nvim +PackerSync
