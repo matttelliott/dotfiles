54bf71e4bd051bd6de5f273b93171aada389e177
matt <git@matttelliott.com>
2022-10-13 23:03:53 -0500

    chromium conflict

--- chromium/setup.mac.sh
+++ chromium/setup.mac.sh
@@ -2,7 +2,7 @@
 cd "$HOME/dotfiles"
 bat chromium/README.md
 stow chromium
-brew install eloston-chromium
-brew install chromium
+brew install eloston-chromium # degoogled chromium
+# brew install chromium # conflicts with eloston-chromium
 brew install google-chrome
 brew install homebrew/cask-versions/google-chrome-canary
