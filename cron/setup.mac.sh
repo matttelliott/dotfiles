0736763151d613195561e7a2a78bd13f9da0ff42
matt <git@matttelliott.com>
2022-10-15 17:53:54 -0500

    hostname cron

--- cron/setup.mac.sh
+++ cron/setup.mac.sh
@@ -4,16 +4,10 @@ bat cron/README.md
 isMattsMacbook=$(hostname | grep -i matt | grep -i macbook)
 isMattsChromebook=$(hostname | grep -i matt | grep -i chromebook)
 isPlutoMacbook=$(hostname | grep -i plutotv)
-echo $isMattsMacbook
-echo $isMattsChromebook
-echo $isPlutoMacbook
 if [ $isMattsMacbook != "" ]; then
-	echo $isMattsChromebook
 	crontab cron/tabs/crontab.macbook
 elif [ $isMattsChromebook != "" ]; then
-	echo $isMattsChromebook
 	crontab cron/tabs/crontab.chromebook
 elif [ $isPlutoMacbook != "" ]; then
-	echo $isPlutoMacbook
-	crontab cron/tabs/crontab.pluto
+	crontab cron/tabs/crontab.plutotv
 fi
