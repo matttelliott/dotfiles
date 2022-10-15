1037e46de0c2499eb63c8f256708b6386fdfc282
matt <git@matttelliott.com>
2022-10-15 17:40:04 -0500

    auto - matt@Matts-MacBook-Air.local

--- cron/setup.mac.sh
+++ cron/setup.mac.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles/"
 bat cron/README.md
-crontab dotfiles/cron/tabs/crontab.macbook
+#crontab cron/tabs/crontab.macbook
+echo '$HOST'
+echo $HOST
+hostname=$(echo $HOST | grep -i matt | grep -i macbook)
+echo $hostname
