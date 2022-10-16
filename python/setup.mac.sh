fe5f2c7cd2d89f08dad4107446d05360da59ba7b
matt <git@matttelliott.com>
2022-10-15 19:29:11 -0500

    pynvim

--- python/setup.mac.sh
+++ python/setup.mac.sh
@@ -18,3 +18,11 @@ pip3 install pynvim
 pip3 install black
 pip3 install flake8
 stow python
+
+cd "$HOME"
+python3 -m venv python-neovim
+cd python-neovim
+source bin/activate
+pip install pynvim
+pip install black
+pip install flake8
