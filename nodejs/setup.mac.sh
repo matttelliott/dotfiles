9b1389a41c8436ec0b81dd099fe7daf7e20724fc
matt <git@matttelliott.com>
2022-10-15 19:50:48 -0500

    sqlformatter

--- nodejs/setup.mac.sh
+++ nodejs/setup.mac.sh
@@ -12,6 +12,7 @@ nvm use --lts
 npm install -g neovim
 npm install -g typescript-language-server
 npm install -g vscode-langservers-extracted
+npm install -g sql-formatter
 npm install -g eslint_d
 npm install -g prettier
 stow nodejs
