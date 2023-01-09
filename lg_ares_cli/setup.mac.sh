e30cb71a96d44267f3dbd27321d38193a47e6c96
Matt Elliott <matt.elliott@pluto.tv>
2023-01-09 14:40:01 -0600

    auto - matt@Matts-MBP

--- lg_ares_cli/setup.mac.sh
+++ lg_ares_cli/setup.mac.sh
@@ -3,3 +3,10 @@ cd "$HOME/dotfiles"
 bat lg_ares_cli/README.md
 stow lg_ares_cli
 npm uninstall -g @webosose/ares-cli
+ares_path=$HOME/lg-webos-cli/bin
+archive_path=$HOME/Downloads/webOS_TV_CLI_mac_1.12.3-j26.tgz
+cd $HOME/Downloads
+open $archive_path
+mkdir -pv $ares_path
+mv CLI/bin/* $ares_path
+
