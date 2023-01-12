8b94879c2ab813123e9300c00c46b54789a0970f
Matt Elliott <matt.elliott@pluto.tv>
2023-01-12 14:50:00 -0600

    auto - matt@Matts-MBP

--- neovim/.config/nvim/lua/user/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -35,45 +35,6 @@ local handle_eslint_output = function(params)
     return parser({ output = params.messages })
 end
 
-return h.make_builtin({
-    name = "eslint",
-    meta = {
-        url = "https://github.com/eslint/eslint",
-        description = "A linter for the JavaScript ecosystem.",
-    },
-    method = DIAGNOSTICS,
-    filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-    generator_opts = {
-        command = "eslint",
-        args = { "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-        to_stdin = true,
-        format = "json_raw",
-        check_exit_code = function(code)
-            return code <= 1
-        end,
-        use_cache = true,
-        on_output = handle_eslint_output,
-        dynamic_command = cmd_resolver.from_node_modules(),
-        cwd = h.cache.by_bufnr(function(params)
-            return u.root_pattern(
-                -- https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new
-                "eslint.config.js",
-                -- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
-                ".eslintrc",
-                ".eslintrc.js",
-                ".eslintrc.cjs",
-                ".eslintrc.yaml",
-                ".eslintrc.yml",
-                ".eslintrc.json",
-                "package.json"
-            )(params.bufname)
-        end),
-    },
-    factory = h.generator_factory,
-})
-
-
-
 
 local eslint_custom = h.make_builtin({
 	name = "eslint",
@@ -96,17 +57,7 @@ local eslint_custom = h.make_builtin({
 		dynamic_command = cmd_resolver.from_node_modules(),
 		cwd = h.cache.by_bufnr(function(params)
 			return u.root_pattern(
-				"package.json",
-				-- https://eslint.org/docs/latest/user-guide/configuring/configuration-files-new
-				"eslint.config.js",
-				-- https://eslint.org/docs/user-guide/configuring/configuration-files#configuration-file-formats
-				".eslintrc",
-				".eslintrc.js",
-				".eslintrc.cjs",
-				".eslintrc.yaml",
-				".eslintrc.yml",
-				".eslintrc.json"
-			)(params.bufname)
+				"package.json"			)(params.bufname)
 		end),
 	},
 	factory = h.generator_factory,
