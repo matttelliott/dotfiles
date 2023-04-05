71f9269a9d88ef088714bbec4388dc016b7a165a
Matt Elliott <matt.elliott@pluto.tv>
2023-04-05 13:40:01 -0500

    auto - matt@Matts-MBP

--- neovim/.config/nvim/lua/user/lsp/null-ls.lua
+++ neovim/.config/nvim/lua/user/lsp/null-ls.lua
@@ -1,8 +1,4 @@
 local null_ls_status_ok, null_ls = pcall(require, "null-ls")
-local h = require("null-ls.helpers")
-local methods = require("null-ls.methods")
-local u = require("null-ls.utils")
-local cmd_resolver = require("null-ls.helpers.command_resolver")
 if not null_ls_status_ok then
 	return
 end
@@ -11,53 +7,6 @@ end
 local formatting = null_ls.builtins.formatting
 -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
 local diagnostics = null_ls.builtins.diagnostics
-
-
-
-local DIAGNOSTICS = methods.internal.DIAGNOSTICS
-
-local handle_eslint_output = function(params)
-    params.messages = params.output and params.output[1] and params.output[1].messages or {}
-    if params.err then
-        table.insert(params.messages, { message = params.err })
-    end
-
-    local parser = h.diagnostics.from_json({
-        attributes = {
-            severity = "severity",
-        },
-        severities = {
-            h.diagnostics.severities["warning"],
-            h.diagnostics.severities["error"],
-        },
-    })
-
-    return parser({ output = params.messages })
-end
-
-
-local eslint_custom = h.make_builtin({
-	name = "eslint",
-	meta = {
-		url = "https://github.com/eslint/eslint",
-		description = "A linter for the JavaScript ecosystem.",
-	},
-	method = DIAGNOSTICS,
-	filetypes = { "javascript", "javascriptreact", "typescript", "typescriptreact", "vue" },
-	generator_opts = {
-		command = "eslint",
-		args = { "-f", "json", "--stdin", "--stdin-filename", "$FILENAME" },
-		to_stdin = true,
-		format = "json_raw",
-		check_exit_code = function(code)
-			return code <= 1
-		end,
-		use_cache = true,
-		on_output = handle_eslint_output,
-		dynamic_command = cmd_resolver.from_node_modules()
-	},
-	factory = h.generator_factory,
-})
 
 null_ls.setup({
 	debug = false,
@@ -68,6 +17,6 @@ null_ls.setup({
 		formatting.shfmt,
 		formatting.sql_formatter,
 		diagnostics.flake8,
-		eslint_custom,
+		diagnostics.eslint,
 	},
 })
