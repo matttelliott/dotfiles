9774ae237c80288ab6b78c5863470ad284b8f3d3
matt <matt+gitlab@matt-elliott.me>
2023-06-05 00:16:48 +0000

    Update file setup.debian.sh

--- rust/setup.debian.sh
+++ rust/setup.debian.sh
@@ -2,12 +2,16 @@
 cd "$HOME/dotfiles"
 curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
 source "$HOME/.cargo/env"
-# cargo install sccache
-# export RUSTC_WRAPPER=sccache cargo install {package}
+
+
 rustup +stable component add rust-analyzer-preview
 cargo install coreutils
-# cargo install cargo-binstall
-cargo install irust -y
-cargo install bacon -y
-cargo install cargo-info -y
+cargo install irust
+cargo install bacon
+cargo install cargo-info
 stow rust
+
+# cargo install sccache
+# export RUSTC_WRAPPER=sccache cargo install {package}
+
+# cargo install cargo-binstall
