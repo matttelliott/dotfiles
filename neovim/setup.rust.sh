382275390091185e43fb2062ea5cd979ec5dd04a
matt <matt+gitlab@matt-elliott.me>
2023-06-06 22:45:55 +0000

    Update file setup.rust.sh

--- neovim/setup.rust.sh
+++ neovim/setup.rust.sh
@@ -13,6 +13,12 @@ gum spin --spinner dot --title "Installing Neovim via nvim-bob" --title.foregrou
 export PATH=$PATH:$HOME/.local/share/bob/nvim-bin
 echo "export PATH=$PATH:$HOME/.local/share/bob/nvim-bin" >> $HOME/.bashrc
 
+title="NvChad"
+description= "More modern NeoVim"
+url="https://github.com/NvChad/NvChad"
+color=15
+gum style --foreground $color --border-foreground $color --border double --align center --width 50 --margin "1 2" --padding "2 4" $title $description $url
+
 gum spin --spinner dot --title "Installing NvChad" --title.foreground $color -- git clone https://github.com/NvChad/NvChad ~/.config/nvim --depth 1
 
 stow neovim
