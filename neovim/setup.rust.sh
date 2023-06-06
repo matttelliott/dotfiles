1c1e77524b050d4525374919cc1b48f13fa19e06
matt <matt+gitlab@matt-elliott.me>
2023-06-06 20:41:13 +0000

    Update file setup.rust.sh

--- neovim/setup.rust.sh
+++ neovim/setup.rust.sh
@@ -1,6 +1,13 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles"
-cat neovim/README.md
+
+
+title="NeoVim \n"
+description= "More modern Vim \n"
+url="https://neovim.io/ \n"
+color=5
+gum style --foreground $color --border-foreground $color --border double --align center --width 50 --margin "1 2" --padding "2 4" $title $description $url
+
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.local/state/nvim
 stow neovim
