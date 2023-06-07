1fff7f34ad840ed2316b92e7b597a443539728ad
matt <matt+gitlab@matt-elliott.me>
2023-06-07 20:58:41 +0000

    Update file setup.debian.sh

--- zsh/setup.debian.sh
+++ zsh/setup.debian.sh
@@ -2,7 +2,7 @@
 # https://github.com/unixorn/awesome-zsh-plugins
 cd "$HOME/dotfiles"
 sudo apt install -y zsh
-rm $HOME/.zshenv
-rm $HOME/.zshrc
+rm -rf $HOME/.zshenv
+rm -rf $HOME/.zshrc
 stow zsh
 sudo chsh -s $(which zsh) $(whoami)
