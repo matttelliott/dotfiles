74581ce1cb5e05d1eb516c9db76c050e6a39f2a6
matt <matt+gitlab@matt-elliott.me>
2023-06-07 05:00:16 +0000

    Update 2 files
    - /nodejs/setup.sh
    - /neovim/.zsh_modules/neovim

--- nodejs/setup.sh
+++ nodejs/setup.sh
@@ -7,7 +7,7 @@ color=5
 gum style --foreground $color --border-foreground $color --border double --align center --width 50 --margin "1 2" --padding "2 4" $title $url
 
 eval "$(rtx activate bash)"
-rtx hook-env
+eval "$(rtx hook-env)"
 gum spin --spinner dot --title "Setting Up NodeJs" --title.foreground $color -- rtx use --global nodejs@lts
 stow nodejs
 
