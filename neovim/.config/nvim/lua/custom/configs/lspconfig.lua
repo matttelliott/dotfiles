e52e47dd02c5a7dcd4befa55566f7553749d50c8
matt <matt@matt-desktop.matt-home>
2023-07-11 15:53:32 -0500

    wip

--- neovim/.config/nvim/lua/custom/configs/lspconfig.lua
+++ neovim/.config/nvim/lua/custom/configs/lspconfig.lua
@@ -3,7 +3,7 @@ local capabilities = require("plugins.configs.lspconfig").capabilities
 
 local lspconfig = require("lspconfig")
 -- if you just want default config for the servers then put them in a table
-local servers = { "html", "cssls", "tsserver", "clangd", "emmet_ls", "angularls", "eslint" }
+local servers = { "html", "cssls", "tsserver", "clangd", "emmet_ls", "angularls", "eslint", "tailwindcss" }
 
 for _, lsp in ipairs(servers) do
 	lspconfig[lsp].setup({
--- neovim/.config/nvim/lua/custom/mappings.lua
+++ neovim/.config/nvim/lua/custom/mappings.lua
@@ -3,8 +3,8 @@ local M = {}
 
 M.general = {
 	n = {
-		["<localleader>w"] = { ":wa<CR>", "write all files" },
-		["<localleader>g"] = { ":tabnew<CR>:terminal lazygit<CR>i", "git gui" },
+		["<localleader>w"] = { ":wa<CR>", "Write all files" },
+		["<localleader>g"] = { ":tabnew<CR>:terminal lazygit<CR>i", "Git gui" },
 
 		["<localleader>d"] = {
 			function()
@@ -21,20 +21,20 @@ M.general = {
 	},
 	i = {
 		["jk"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
-		["kj"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
 		["JK"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
 		["jK"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
 		["Jk"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
-		["KJ"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
-		["kJ"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
-		["Kj"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
+		-- ["KJ"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
+		-- ["kJ"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
+		-- ["Kj"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
+		-- ["kj"] = { "<esc>", "enter normal mode", opts = { nowait = true } },
 	},
 }
 
 M.telescope = {
 	plugin = true,
 	n = {
-		["<localleader>o"] = { "<cmd> Telescope find_files hidden=true<CR>", "Find files" },
+		["<localleader>o"] = { "<cmd> Telescope find_files hidden=true<CR>", "Open file" },
 		["<localleader>/"] = { "<cmd> Telescope live_grep hidden=true<CR>", "Live grep" },
 	},
 }
@@ -42,7 +42,8 @@ M.telescope = {
 M.nvimtree = {
 	plugin = true,
 	n = {
-		["<localleader>e"] = { "<cmd> NvimTreeToggle <CR>", "Focus nvimtree" },
+		["<localleader>e"] = { "<cmd> NvimTreeToggle <CR>", "toggle Explorer" },
+		["<localleader>E"] = { "<cmd> NvimTreeFindFile <CR>", "find current file in Explorer" },
 	},
 }
 -- more keybinds!
