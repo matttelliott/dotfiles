1a24eec85b1d314b139ce74ef6083c62f0adffbe
Matt Elliott <matt@matt-elliott.me>
2024-11-15 16:50:27 -0600

    wip

--- neovim/.config/nvim/lua/custom/configs/null-ls.lua
+++ neovim/.config/nvim/lua/custom/configs/null-ls.lua
@@ -1,1 +1,33 @@
-../../../../../dotfiles/neovim/.config/nvim/lua/custom/configs/null-ls.lua
+local present, null_ls = pcall(require, "null-ls")
+
+if not present then
+  return
+end
+
+local b = null_ls.builtins
+
+local sources = {
+
+  -- webdev stuff
+  -- b.formatting.deno_fmt, -- choosed deno for ts/js files cuz its very fast!
+  b.formatting.prettier, --.with { filetypes = { "html", "markdown", "css" } }, -- so prettier works only on these filetypes
+
+  -- sql
+  b.formatting.sqlfmt,
+
+  -- Lua
+  b.formatting.stylua,
+
+  -- cpp
+  b.formatting.clang_format,
+
+  -- python
+  b.formatting.black,
+
+  b.formatting.gofumpt,
+}
+
+null_ls.setup {
+  debug = true,
+  sources = sources,
+}
--- neovim/.config/nvim/lua/custom/configs/overrides.lua
+++ neovim/.config/nvim/lua/custom/configs/overrides.lua
@@ -1,1 +1,101 @@
-../../../../../dotfiles/neovim/.config/nvim/lua/custom/configs/overrides.lua
+local M = {}
+
+M.treesitter = {
+	ensure_installed = {
+		"vim",
+		"lua",
+		"html",
+		"css",
+		"javascript",
+		"typescript",
+		"tsx",
+		"rust",
+		"python",
+		"bash",
+		"markdown",
+		"markdown_inline",
+		"htmldjango",
+	},
+	indent = {
+		enable = true,
+		-- disable = {
+		--   "python"
+		-- },
+	},
+}
+
+M.mason = {
+	ensure_installed = {
+		-- lua stuff
+		"lua-language-server",
+		"stylua",
+
+		-- web dev stuff
+		"css-lsp",
+		"html-lsp",
+		"typescript-language-server",
+		"angular-language-server",
+		"deno",
+		"prettier",
+		"angular-language-server",
+		"emmet-ls",
+
+		-- docker
+		"docker-compose-language-service",
+		"dockerfile-language-server",
+
+		-- shell stuff
+		"bash-debug-adapter",
+		"bash-language-server",
+
+		-- json/yaml/toml
+		"json-lsp",
+		"taplo",
+		"yaml-language-server",
+
+		--rust
+		"rust-analyzer",
+		"rustfmt",
+		"codelldb",
+
+		--sql
+		"sqlls",
+		"sqlfmt",
+
+		--python
+		"mypy",
+		"jedi-language-server",
+	},
+}
+
+-- git support in nvimtree
+M.nvimtree = {
+	git = {
+		enable = true,
+	},
+
+	renderer = {
+		highlight_git = true,
+		icons = {
+			show = {
+				git = true,
+			},
+		},
+	},
+}
+
+M.telescope = {
+	vimgrep_arguments = {
+		"rg",
+		"-L",
+		"--color=never",
+		"--no-heading",
+		"--with-filename",
+		"--line-number",
+		"--column",
+		"--smart-case",
+		"--hidden",
+	},
+}
+
+return M
