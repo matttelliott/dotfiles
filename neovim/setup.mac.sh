ff5b2d87ad1145a54487bc83e6a662acbb2f7fcb
Matt Elliott <matt@matt-elliott.me>
2024-11-19 14:36:32 -0600

    wip

--- neovim/setup.mac.sh
+++ neovim/setup.mac.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 set -v
 cd "$HOME/dotfiles"
+cat neovim/README.md
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.local/state/nvim
 stow --no-folding neovim
 rm -rf $HOME/.local/share/nvim
 rm -rf $HOME/.local/state/nvim
 brew install neovim
-echo 'install neovim packages'
-nvim +PackerSync +qa!
-sleep 2
-echo 'installed neovim'
-cat neovim/README.md
+# echo 'install neovim packages'
+# nvim +PackerSync +qa!
+# sleep 2
+# echo 'installed neovim'
