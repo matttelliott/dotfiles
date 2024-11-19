212fe3f3c3809f2392c0633f3deeface3929f0d1
Matt Elliott <matt@matt-elliott.me>
2024-11-19 15:06:58 -0600

    wip

--- chromium/setup.mac.sh
+++ chromium/setup.mac.sh
@@ -3,5 +3,5 @@ cd "$HOME/dotfiles"
 brew install eloston-chromium # degoogled chromium
 # brew install chromium # conflicts with eloston-chromium
 brew install google-chrome
-brew install homebrew/cask-versions/google-chrome-canary
+brew install google-chrome-canary
 stow --no-folding chromium
