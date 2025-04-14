b07e23f9de8b96ad154559a41124702d411d1816
Matt Elliott <matt@matt-elliott.me>
2025-04-14 16:14:35 -0500

    wip

--- fonts/setup.mac.sh
+++ fonts/setup.mac.sh
@@ -1,13 +1,13 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles"
 
-brew install --cask font-inconsolata || true
-brew install --cask font-hack-nerd-font || true
-brew install --cask font-dejavu-sans-mono-for-powerline || true
-brew install --cask font-dejavu-sans-mono-nerd-font || true
-brew install --cask font-hack-nerd-font || true
-brew install --cask font-awesome-terminal-fonts || true
-brew install --cask font-fontawesome || true
+brew install --cask font-inconsolata
+brew install --cask font-hack-nerd-font
+brew install --cask font-dejavu-sans-mono-for-powerline
+brew install --cask font-dejavu-sans-mono-nerd-font
+brew install --cask font-hack-nerd-font
+brew install --cask font-awesome-terminal-fonts
+brew install --cask font-fontawesome
 
 stow --no-folding fonts
 cp $HOME/.local/share/fonts/* /Library/Fonts/
--- tmux/setup.mac.sh
+++ tmux/setup.mac.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 cd "$HOME/dotfiles"
 cat tmux/README.md
-brew install tmux || true
+brew install tmux
 stow --no-folding tmux
